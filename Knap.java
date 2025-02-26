2b741ad8de2cc2e08242e64e052b147dadd899b7
KathrineLoll <162918132+KathrineLoll@users.noreply.github.com>
2025-02-26 12:59:22 +0100

    knap ting

--- Knap.java
+++ Knap.java
@@ -1,1 +1,51 @@
+import processing.core.*;
+public class knap{
 
+    private PApplet p;
+
+    private int x, y,b,h;
+    private String titel;
+    private p.color rectColor, rectHighlight;
+    private String action;
+    
+    public knap(PApplet p, int xin, int yin, int bin, int hin, String titelin, String actionin){
+      this.p = p;
+      x = xin; 
+      y = yin; 
+      b = bin; 
+      h = hin;
+      titel = titelin; 
+      action = actionin;
+      
+      rectColor = p.color(200);
+      rectHighlight = p.color(100); 
+    }
+
+    public void display(){
+        if(p.mouseX < x + b && p.mouseX > x && p.mouseY < y + h && p.mouseY > y){
+         fill(rectHighlight);
+        } 
+        else {
+          fill(rectColor);
+        }
+        noStroke();
+        rect(x,y,b,h,25);
+
+        textAlign(p.LEFT);
+        fill(50);
+        textSize(15);
+        float w = textWidth(titel);
+        text(titel,x + b/2 - w/2,y + h/2 + 5);
+        
+    
+      }
+      
+      private void mouseClickDetection(){
+       if(p.mouseX < x + b && p.mouseX > x && p.mouseY < y + h && p.mouseY > y){
+         method(action);
+       }
+      }
+    
+
+
+}
